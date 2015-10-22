6cd3f7ae884eb32d1067f8f13962c07d90617746
Erin Sparling <erin@everyplace.net>
2015-10-22 20:41:01 +0100

    Converted all variables to environmental variables

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.listen(process.env.PORT || 5000);
 //
 // Config.keys uses environment variables so sensitive info is not in the repo.
 var config = {
-    me: 'StoriesInData', // The authorized account with a list to retweet.
-    myList: 'data-driven-storytellers', // The list we want to retweet.
-    regexFilter: '', // Accept only tweets matching this regex pattern.
-    regexReject: '(RT|@)', // AND reject any tweets matching this regex pattern.
+    me: process.env.TWITTER_ACCOUNT || 'StoriesInData', // The authorized account with a list to retweet.
+    myList: process.env.TWITTER_LIST 'data-driven-storytellers', // The list we want to retweet.
+    regexFilter: process.env.REGEX_FILTER || '', // Accept only tweets matching this regex pattern.
+    regexReject: process.env.REGEX_REJECT || '(RT|@)', // AND reject any tweets matching this regex pattern.
 
     keys: {
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
